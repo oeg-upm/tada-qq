6f2331da5999af3fc157987133cf641116a53572
Ahmad Alobaid <ahmad88me@gmail.com>
2022-05-04 11:58:05 +0200

    add clus pref option with more tests

--- tadaqq/slabmer/slabmer.py
+++ tadaqq/slabmer/slabmer.py
@@ -5,19 +5,14 @@ from tadaqq.util import uri_to_fname, compute_scores
 
 class SLabMer:
 
+    SLAB_PREF = 'slabel_pref'
+    CLUS_PREF = 'clus_pref'
+    PREFS = [SLAB_PREF, CLUS_PREF]
+
     def __init__(self, endpoint):
         self.sl = SLabel(endpoint)
 
-    def annotate_cluster(self, group, remove_outliers, estimate, err_meth, candidate_failback, k=3):
-        """
-        Store the predicted properties and assign a single candidate for each element in the given group
-        :param group:
-        :param remove_outliers:
-        :param estimate:
-        :param err_meth:
-        :param k:
-        :return:
-        """
+    def _assign_preds_and_get_freq(self, group, remove_outliers, estimate, err_meth, k=3):
         pred_classes = []
         for ele in group:
             self.sl.collect_numeric_data(class_uri=ele['class_uri'])
@@ -28,22 +23,85 @@ class SLabMer:
             ele['candidates'] = []
             for p in ele['preds']:
                 pred_classes.append(p)
+
         c = Counter(pred_classes)
         freqs = c.most_common()
+        return freqs
 
+    def annotate_cluster(self, group, remove_outliers, estimate, err_meth, candidate_failback, pref=None, k=3):
+        """
+        Store the predicted properties and assign a single candidate for each element in the given group
+        :param group:
+        :param remove_outliers:
+        :param estimate:
+        :param err_meth:
+        :param k:
+        :return:
+        """
+        if pref not in self.PREFS:
+            print("Invalid preference")
+            return None
+
+        freqs = self._assign_preds_and_get_freq(group=group, remove_outliers=remove_outliers, estimate=estimate,
+                                                     err_meth=err_meth, k=k)
+        if pref == self.SLAB_PREF:
+            return self.annotate_cluster_slabel_pref(group=group, freqs=freqs, candidate_failback=candidate_failback)
+        elif pref == self.CLUS_PREF:
+            return self.annotate_cluster_clus_pref(group=group, freqs=freqs)
+
+    def annotate_cluster_clus_pref(self, group, freqs):
+        """
+        Store the predicted properties and assign a single candidate for each element in the given group
+
+        :param group:
+        :param freqs: a list of the pairs (property uri, number of occurrences)
+        :return:
+        """
+        # print("\n\nGroup: ")
+        for ele in group:
+            for prop_uri, fre in freqs:
+                ele['candidates'].append(prop_uri)
+
+            # print("\tclass uri: %s" % ele['class_uri'])
+            # print("\tcol id: %d" % ele['col_id'])
+            # print("\tfname: %s" % ele['fname'])
+            # print("\tproperty: %s " % ele['property'])
+            # print("\tcandidate: %s " % str(ele['candidates'][:3]))
+            # print("===============================")
+
+    def annotate_cluster_slabel_pref(self, group, freqs, candidate_failback):
+        """
+        Store the predicted properties and assign a single candidate for each element in the given group
+        :param group:
+        :param remove_outliers:
+        :param estimate:
+        :param err_meth:
+        :param k:
+        :return:
+        """
+
+        # print("\n\nGroup: ")
         for ele in group:
             for prop_uri, fre in freqs:
                 if prop_uri in ele['preds']:
                     ele['candidates'].append(prop_uri)
+
+            # print("\tclass uri: %s" % ele['class_uri'])
+            # print("\tcol id: %d" % ele['col_id'])
+            # print("\tfname: %s" % ele['fname'])
+            # print("\tproperty: %s " % ele['property'])
+            # print("\tcandidate: %s " % str(ele['candidates'][:3]))
+            # print("===============================")
 
             if candidate_failback:
                 for p in ele['preds']:
                     if p not in ele['candidates']:
                         ele['candidates'].append(p)
 
-    def annotate_clusters(self, groups, remove_outliers, estimate, err_meth, candidate_failback, k=3):
+    def annotate_clusters(self, groups, remove_outliers, estimate, err_meth, candidate_failback, pref=SLAB_PREF, k=3):
         for g in groups:
-            self.annotate_cluster(g, remove_outliers, estimate, err_meth, candidate_failback, k)
+            self.annotate_cluster(group=g, remove_outliers=remove_outliers, estimate=estimate, err_meth=err_meth,
+                                  candidate_failback=candidate_failback, k=k, pref=pref)
 
     def evaluate_labelling(self, groups):
         """
--- tests/test_slabmer.py
+++ tests/test_slabmer.py
@@ -43,29 +43,175 @@ class SLabMerTest(unittest.TestCase):
     def setUp(self) -> None:
         pass
 
-    def test_slabmer_perfect(self):
+    def test_slabmer_slab_perfect(self):
         base_dir = os.path.join('tests', 'test_files')
         meta_df = pd.read_csv(os.path.join(base_dir, 'meta-basketball.csv'))
         clusterer = apply_cluster(meta_df, fetch_method="max", err_meth="mean_err", err_cutoff=0.15, same_class=False)
         self.assertEqual(len(clusterer.groups), 2)
         slabmer = SLabMer(SPARQL_ENDPOINT)
         slabmer.annotate_clusters(clusterer.groups, remove_outliers=False, estimate=True, err_meth="mean_err",
-                                  candidate_failback=False, k=1)
+                                  candidate_failback=False, k=1, pref=SLabMer.SLAB_PREF)
         score = slabmer.evaluate_labelling(clusterer.groups)
         self.assertEqual(score['prec'], 1.0)
         self.assertEqual(score['rec'], 1.0)
         self.assertEqual(score['f1'], 1.0)
 
-    def test_slabmer_precision(self):
+    def test_slabmer_slab_precision(self):
         base_dir = os.path.join('tests', 'test_files')
         meta_df = pd.read_csv(os.path.join(base_dir, 'meta-mountain.csv'))
+        clusterer = apply_cluster(meta_df, fetch_method="max", err_meth="mean_err", err_cutoff=0.30, same_class=False)
+        self.assertEqual(len(clusterer.groups), 2)
+        slabmer = SLabMer(SPARQL_ENDPOINT)
+        slabmer.annotate_clusters(clusterer.groups, remove_outliers=False, estimate=True, err_meth="mean_err",
+                                  candidate_failback=False, k=100, pref=SLabMer.SLAB_PREF)
+
+        score = slabmer.evaluate_labelling(clusterer.groups)
+        self.assertEqual(score['prec'], 0.75)
+        self.assertEqual(score['rec'], 1.0)
+
+    def test_slabmer_slab_precision_recall(self):
+        base_dir = os.path.join('tests', 'test_files')
+        meta_df = pd.read_csv(os.path.join(base_dir, 'meta-mountain.csv'))
+        clusterer = apply_cluster(meta_df, fetch_method="max", err_meth="mean_err", err_cutoff=0.30, same_class=False)
+        self.assertEqual(len(clusterer.groups), 2)
+        slabmer = SLabMer(SPARQL_ENDPOINT)
+        slabmer.annotate_clusters(clusterer.groups, remove_outliers=False, estimate=True, err_meth="mean_err",
+                                  candidate_failback=False, k=100, pref=SLabMer.SLAB_PREF)
+        clusterer.groups[0][0]['candidates'] = []  # To evaluate the recall formula
+        clusterer.groups[0][1]['candidates'] = []  # To evaluate the recall formula
+
+        # for group in clusterer.groups:
+        #     print("\n\nGroup: ")
+        #     for ele in group:
+        #         print("\tclass uri: %s" % ele['class_uri'])
+        #         print("\tcol id: %d" % ele['col_id'])
+        #         print("\tfname: %s" % ele['fname'])
+        #         print("\tproperty: %s " % ele['property'])
+        #         print("\tcandidate: %s " % str(ele['candidates'][:3]))
+        #         print("===============================")
+
+        score = slabmer.evaluate_labelling(clusterer.groups)
+        self.assertEqual(score['rec'], 0.5)
+
+    def test_slabmer_clus_perfect(self):
+        base_dir = os.path.join('tests', 'test_files')
+        meta_df = pd.read_csv(os.path.join(base_dir, 'meta-basketball.csv'))
         clusterer = apply_cluster(meta_df, fetch_method="max", err_meth="mean_err", err_cutoff=0.15, same_class=False)
         self.assertEqual(len(clusterer.groups), 2)
         slabmer = SLabMer(SPARQL_ENDPOINT)
         slabmer.annotate_clusters(clusterer.groups, remove_outliers=False, estimate=True, err_meth="mean_err",
-                                  candidate_failback=False, k=100)
+                                  candidate_failback=False, k=1, pref=SLabMer.CLUS_PREF)
         score = slabmer.evaluate_labelling(clusterer.groups)
-        self.assertLess(score['prec'], 0.7)
+        self.assertEqual(score['prec'], 1.0)
+        self.assertEqual(score['rec'], 1.0)
+        self.assertEqual(score['f1'], 1.0)
+
+    def test_slabmer_clus_precision(self):
+        base_dir = os.path.join('tests', 'test_files')
+        meta_df = pd.read_csv(os.path.join(base_dir, 'meta-mountain.csv'))
+        clusterer = apply_cluster(meta_df, fetch_method="max", err_meth="mean_err", err_cutoff=0.35, same_class=False)
+        self.assertEqual(len(clusterer.groups), 2)
+        slabmer = SLabMer(SPARQL_ENDPOINT)
+        slabmer.annotate_clusters(clusterer.groups, remove_outliers=False, estimate=True, err_meth="mean_err",
+                                  candidate_failback=False, k=100, pref=SLabMer.CLUS_PREF)
+        score = slabmer.evaluate_labelling(clusterer.groups)
+
+        self.assertEqual(score['prec'], 0.75)
+        self.assertEqual(score['rec'], 1.0)
+
+    def test_slabmer_clus_recall(self):
+        base_dir = os.path.join('tests', 'test_files')
+        meta_df = pd.read_csv(os.path.join(base_dir, 'meta-mountain.csv'))
+        clusterer = apply_cluster(meta_df, fetch_method="max", err_meth="mean_err", err_cutoff=0.35, same_class=False)
+        self.assertEqual(len(clusterer.groups), 2)
+        slabmer = SLabMer(SPARQL_ENDPOINT)
+        slabmer.annotate_clusters(clusterer.groups, remove_outliers=False, estimate=True, err_meth="mean_err",
+                                  candidate_failback=False, k=100, pref=SLabMer.CLUS_PREF)
+        clusterer.groups[0][0]['candidates'] = []  # To evaluate the recall formula
+        clusterer.groups[0][1]['candidates'] = []  # To evaluate the recall formula
+        score = slabmer.evaluate_labelling(clusterer.groups)
+
+        self.assertEqual(score['rec'], 0.5)
+
+    def test_slabmer_perfs_slabel(self):
+        base_dir = os.path.join('tests', 'test_files')
+        meta_df = pd.read_csv(os.path.join(base_dir, 'meta-mountain.csv'))
+        clusterer = apply_cluster(meta_df, fetch_method="max", err_meth="mean_err", err_cutoff=0.35, same_class=False)
+        self.assertEqual(len(clusterer.groups), 2)
+        slabmer = SLabMer(SPARQL_ENDPOINT)
+        # slabmer.annotate_clusters(clusterer.groups, remove_outliers=False, estimate=True, err_meth="mean_err",
+        #                           candidate_failback=False, k=100, pref=SLabMer.CLUS_PREF)
+        err_meth = "mean_err"
+        estimate = True
+        remove_outliers = False
+
+        fake_path = 'local_data/dbo-fake/dbp-fake.txt'
+        fake_prop_uri = 'http://dbpedia.org/property/fake'
+        fake_class_uri = 'http://dbpedia.org/ontology/fake'
+
+        for idx, g in enumerate(clusterer.groups):
+            freqs = slabmer._assign_preds_and_get_freq(group=g, remove_outliers=remove_outliers, estimate=estimate,
+                                                       err_meth=err_meth)
+            if idx == 0:
+                for ele in g:
+                    # ele['properties'][0] = fake_prop_uri
+                    # ele['property'] = fake_prop_uri
+                    ele['class_uri'] = fake_class_uri
+
+
+                freqs[0] = (fake_path, 1000)
+            slabmer.annotate_cluster_slabel_pref(group=g, freqs=freqs, candidate_failback=False)
+
+        # for group in clusterer.groups:
+        #     print("\n\nGroup: ")
+        #     for ele in group:
+        #         print("\tclass uri: %s" % ele['class_uri'])
+        #         print("\tcol id: %d" % ele['col_id'])
+        #         print("\tfname: %s" % ele['fname'])
+        #         print("\tproperty: %s " % ele['property'])
+        #         print("\tcandidate: %s " % str(ele['candidates'][:3]))
+        #         print("===============================")
+
+        score = slabmer.evaluate_labelling(clusterer.groups)
+        self.assertEqual(score['prec'], 1.0)
+        self.assertEqual(score['rec'], 0.75)
+
+    def test_slabmer_perfs_clus(self):
+        base_dir = os.path.join('tests', 'test_files')
+        meta_df = pd.read_csv(os.path.join(base_dir, 'meta-mountain.csv'))
+        clusterer = apply_cluster(meta_df, fetch_method="max", err_meth="mean_err", err_cutoff=0.35, same_class=False)
+        self.assertEqual(len(clusterer.groups), 2)
+        slabmer = SLabMer(SPARQL_ENDPOINT)
+        err_meth = "mean_err"
+        estimate = True
+        remove_outliers = False
+
+        fake_path = 'local_data/dbo-fake/dbp-fake.txt'
+        fake_prop_uri = 'http://dbpedia.org/property/fake'
+        fake_class_uri = 'http://dbpedia.org/ontology/fake'
+
+        for g in clusterer.groups:
+            freqs = slabmer._assign_preds_and_get_freq(group=g, remove_outliers=remove_outliers, estimate=estimate,
+                                                       err_meth=err_meth)
+            for ele in g:
+                ele['properties'][0] = fake_prop_uri
+                ele['class_uri'] = fake_class_uri
+
+            freqs[0] = (fake_path, 1000)
+            slabmer.annotate_cluster_clus_pref(group=g, freqs=freqs)
+
+        # for group in clusterer.groups:
+        #     print("\n\nGroup: ")
+        #     for ele in group:
+        #         print("\tclass uri: %s" % ele['class_uri'])
+        #         print("\tcol id: %d" % ele['col_id'])
+        #         print("\tfname: %s" % ele['fname'])
+        #         print("\tproperty: %s " % ele['property'])
+        #         print("\tcandidate: %s " % str(ele['candidates'][:3]))
+        #         print("===============================")
+
+        score = slabmer.evaluate_labelling(clusterer.groups)
+        self.assertEqual(score['prec'], 1.0)
         self.assertEqual(score['rec'], 1.0)
 
 
